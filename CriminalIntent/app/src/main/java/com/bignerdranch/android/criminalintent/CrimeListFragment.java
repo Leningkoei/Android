2124ac6663bf748ce3c2dd8c9df750d48f9479ec
Leningkoei <zhanglening2001@gmail.com>
2021-11-11 18:15:42 +0900

    2021/11/11

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class CrimeListFragment extends Fragment {
 
@@ -18,6 +22,7 @@ public class CrimeListFragment extends Fragment {
 
     private CrimeListViewModel crimeListViewModel = null;
     private RecyclerView crimeRecyclerView = null;
+    private CrimeAdapter crimeAdapter = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,17 +44,63 @@ public class CrimeListFragment extends Fragment {
         );
 
         this.crimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
-        LinearLayoutManager linerLayoutManager
-            = new LinearLayoutManager(this.getContext());
+        // LinearLayoutManager linerLayoutManager
+        //     = new LinearLayoutManager(this.getContext());
+        // this.crimeRecyclerView.setLayoutManager(linerLayoutManager);
+        this.crimeRecyclerView.setLayoutManager(
+            new LinearLayoutManager(this.getContext())
+        );
+        this.updateUI();
+
         return view;
     }
 
-    /**
-     * // lang === Kotlin;
-     * companion object {
-     *     fun newInstance(): CrimeListFragment {
-     *         return CrimeListFragment();
-     *     }
-     * }
-     */
+    private void updateUI() {
+        List<Crime> crimes = this.crimeListViewModel.crimes;
+        this.crimeAdapter = new CrimeAdapter(crimes);
+        this.crimeRecyclerView.setAdapter(crimeAdapter);
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+
+        public TextView textView = null;
+        public TextView dateTextView = null;
+
+        // Constructor matching super;
+        public CrimeHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.textView = itemView.findViewById(R.id.crime_title);
+            this.dateTextView = itemView.findViewById(R.id.crime_date);
+        }
+    }
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+
+        private List<Crime> crimes = null;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            this.crimes = crimes;
+        }
+
+        // 至少要覆盖3个函数;
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(
+                R.layout.list_item_crime,
+                parent,
+                false
+            );
+            return new CrimeHolder(view);
+        }
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = this.crimes.get(position);
+            holder.textView.setText(crime.title);
+            holder.dateTextView.setText(crime.date.toString());
+        }
+        @Override
+        public int getItemCount() {
+            return this.crimes.size();
+        }
+    }
 }
